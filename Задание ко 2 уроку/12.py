6de7d07f1c8e5926c9e4c0b3f99d92ad9a214237
9ZonD6 <32245323+9ZonDG6@users.noreply.github.com>
2024-04-20 02:14:37 +0500

    Update 12.py

--- Задание ко 2 уроку/12.py
+++ Задание ко 2 уроку/12.py
@@ -3,5 +3,5 @@ text = input("Введите числа через запятую: ")
 text_tuple = tuple(map(int, text.split(",")))
 text_list = list(text_tuple)
 
-print("Кортеж: ", text_tuple)
-print("Список: ", text_list)
+print(f"Кортеж: {text_tuple}")
+print(f"Список: {text_list}")
