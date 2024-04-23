6345c6cc8a3093550a4538eb345865174d0b02f9
9ZonD6 <32245323+9ZonDG6@users.noreply.github.com>
2024-04-23 17:22:03 +0500

    Update 15.py

--- Задание ко 2 уроку/15.py
+++ Задание ко 2 уроку/15.py
@@ -4,4 +4,4 @@ pyramid = ''
 
 for i in range(2, high + 2):
     pyramid = i * 'x'
-    print(pyramid.replace('x', 'X')[:1] + pyramid.replace('X', 'x')[1:-1] + pyramid.replace('x', 'X')[-1:])
+    print(pyramid.replace('x', 'X')[:1] + pyramid[1:-1] + pyramid.replace('x', 'X')[-1:])
