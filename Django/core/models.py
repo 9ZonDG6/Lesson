a2d07a23aa081b39d79aea008347f0b69a022e3b
9ZonD6 <32245323+9ZonDG6@users.noreply.github.com>
2024-05-06 23:07:10 +0500

    Add files via upload

--- Django/core/models.py
+++ Django/core/models.py
@@ -3,18 +3,34 @@ from django.db.models import CharField
 
 
 class Car(models.Model):
-    name = models.CharField('Название', max_length=100)
+
+    TYPE_CHOICES = (
+        ('petrol', 'Бензин'),
+        ('diesel', 'Дизель'),
+        ('electricity', 'Электричество'),
+        ('hybrid', 'Гибрид'),
+    )
+
+    brand = models.CharField('Марка', max_length=40)
+    model = models.CharField('Модель', max_length=40)
+    fuel = models.CharField('Топливо', max_length=15, choices=TYPE_CHOICES, default='patrol')
+    information = models.TextField('Информация', blank=True)
+    horsepower = models.PositiveSmallIntegerField('Лошадиная сила', default=0)
+    released = models.BooleanField('Выпускается', default=True)
 
     class Meta:
         verbose_name = 'Машина'
         verbose_name_plural = 'Машины'
 
-    def __str__(self) -> CharField:
-        return self.name
+    def __str__(self) -> str:
+        return f'{self.brand} {self.model}'
 
 
 class Plane(models.Model):
+
     name = models.CharField('Название', max_length=100)
+    capacity = models.PositiveSmallIntegerField('Вместимость', default=0)
+    passenger = models.BooleanField('Пассажирский', default=True)
 
     class Meta:
         verbose_name = 'Самолет'
